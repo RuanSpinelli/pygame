b8c8fae0ce05e1bf89a59dbebbea9010692045ae
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2024-09-08 07:23:47 -0300

    update movendo retangulo

--- 01.py
+++ 01.py
@@ -7,14 +7,19 @@ from sys import exit #importa a função do sistema de tornar o "x" das janelas 
 pygame.init() #inicializar o pygame
 largura = 640
 altura = 480
+x =  largura/2
+y = 0
 
 tela = pygame.display.set_mode((largura, altura)) #Definindo as dimensões da janela
 pygame.display.set_caption("jogo") #definindo o titulo da janela do jogo
-
+relogio = pygame.time.Clock() #criando um relogio dentro do jogo
 
 
 while True: #loop principal do jogo
-
+    
+    relogio.tick(30) #definindo quanto será a taxa de atualização do jogo
+    
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -24,12 +29,14 @@ while True: #loop principal do jogo
     #função para desenhar retangulo
 
     #rect(aonde vai ser desenhado, (o esquema RPG ), (posição x, posicão y, largura, altura))
-    pygame.draw.rect(tela, (255,0,0), (200, 300,40,80))
-    #para desenhar circulos se define o raio ali no 40
-    pygame.draw.circle(tela, (0,0,255),(300,260),40)
+    pygame.draw.rect(tela, (255,0,0), (x, y,40,80))
+    
+#lógica para fazer o retangulo sumir
+    if y >= altura:
+        y = 1
+    y += 1
 
-    #para desenhar linha se define a cor, aoonde começa e aonde termina
-    pygame.draw.line(tela, (255,255,0), (390,0),(390,600), 5)
+
 
 
     pygame.display.update()
